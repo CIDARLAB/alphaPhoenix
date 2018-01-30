4964a4ab49fc67438623a3847848d3cad829fb85
PrashantVaidyanathan <prash@bu.edu>
2018-01-29 22:47:53 -0500

    created a script to create a test library

--- src/test/java/org/cidarlab/phoenix/examples/ConstitutiveExpressionTest.java
+++ src/test/java/org/cidarlab/phoenix/examples/ConstitutiveExpressionTest.java
@@ -7,7 +7,9 @@ package org.cidarlab.phoenix.examples;
 
 import java.util.List;
 import org.cidarlab.phoenix.adaptors.MiniEugeneAdaptor;
+import org.cidarlab.phoenix.dom.Component;
 import org.cidarlab.phoenix.dom.Module;
+import org.cidarlab.phoenix.dom.Orientation;
 import org.cidarlab.phoenix.utils.Utilities;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -28,6 +30,32 @@ public class ConstitutiveExpressionTest {
         
         List<Module> modules = MiniEugeneAdaptor.getStructures(eugFilepath, 4, "constExpression");
         assertEquals(modules.size(),1);
+        Module m = modules.get(0);
+        
+        Component p = new Component();
+        p.setRole(Component.ComponentRole.PROMOTER_CONSTITUTIVE);
+        p.setOrientation(Orientation.FORWARD);
+        
+        Component r = new Component();
+        r.setRole(Component.ComponentRole.RBS);
+        r.setOrientation(Orientation.FORWARD);
+        
+        Component c = new Component();
+        c.setRole(Component.ComponentRole.CDS);
+        c.setOrientation(Orientation.FORWARD);
+        
+        Component t = new Component();
+        t.setRole(Component.ComponentRole.TERMINATOR);
+        t.setOrientation(Orientation.FORWARD);
+        
+        assertEquals(m.getComponents().get(0).getRole(),Component.ComponentRole.PROMOTER_CONSTITUTIVE);
+        assertEquals(m.getComponents().get(1).getRole(),Component.ComponentRole.RBS);
+        assertEquals(m.getComponents().get(2).getRole(),Component.ComponentRole.CDS);
+        assertEquals(m.getComponents().get(3).getRole(),Component.ComponentRole.TERMINATOR);
+        
+        String str = "f" + p.getRole().toString() + ";" + "f" + r.getRole().toString() + ";" + "f" +  c.getRole().toString() + ";" + "f" + t.getRole().toString() + ";";
+        
+        assertEquals(str,m.getComponentString());
     }
     
     
