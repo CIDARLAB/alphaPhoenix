bc3b34f473dfceac83ea6fbfc22f21693233e88b
PrashantVaidyanathan <prash@bu.edu>
2018-02-07 02:37:58 -0500

    restructuring for dnaplotlib.

--- src/test/java/org/cidarlab/phoenix/examples/ConstitutiveExpressionTest.java
+++ src/test/java/org/cidarlab/phoenix/examples/ConstitutiveExpressionTest.java
@@ -25,8 +25,8 @@ import org.junit.Test;
 public class ConstitutiveExpressionTest {
     
     
-    private static String rootFolder = Utilities.getFilepath() + Utilities.getSeparater() + "examples" + Utilities.getSeparater() + "tested_circuits" + Utilities.getSeparater();
-    private static String expRootFolder = rootFolder + "constitutive_expression" + Utilities.getSeparater();
+    private static final String rootFolder = Utilities.getFilepath() + Utilities.getSeparater() + "lib" + Utilities.getSeparater()+ "examples" + Utilities.getSeparater() + "tested_circuits" + Utilities.getSeparater();
+    private static final String expRootFolder = rootFolder + "constitutive_expression" + Utilities.getSeparater();
     private static String sburl = "https://synbiohub.cidarlab.org/public/AlphaTest/AlphaTest_collection/1";
     private static String eugFilepath = expRootFolder + "constitutiveExpression.eug";
     private static String stlFilepath = expRootFolder + "stl.txt";
