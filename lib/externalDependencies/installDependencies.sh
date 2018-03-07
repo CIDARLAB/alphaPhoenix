6229dfca92bf68c10bc7e412855d80d08a433ba3
Greg Frasco <frascog@bu.edu>
2018-03-07 12:31:27 -0500

    Need Updated Dependancies

--- lib/externalDependencies/installDependencies.sh
+++ lib/externalDependencies/installDependencies.sh
@@ -1,2 +1,5 @@
 mvn install:install-file -Dfile=miniEugene-core-1.0.0-jar-with-dependencies.jar -DgroupId=org.cidarlab -DartifactId=miniEugene -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=iBioSim.jar -DgroupId=org -DartifactId=ibiosim -Dversion=2.9 -Dpackaging=jar
+mvn install:install-file -Dfile=JavaPlot.jar -DgroupId=org.java.plot -DartifactId=JavaPlot -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=STLSharp.jar -DgroupId=org.cidarlab.bioCPS -DartifactId=STLSharp -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=GridTLI-1.0.jar -DgroupId=org.cidarlab -DartifactId=GridTLI -Dversion=1.0 -Dpackaging=jar
