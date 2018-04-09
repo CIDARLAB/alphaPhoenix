830e197b9bd63a4efd4ff4a4c117c8d66eea8222
PrashantVaidyanathan <prash@bu.edu>
2018-04-09 14:33:09 -0400

    install script updated.

--- lib/externalDependencies/installDependencies.sh
+++ lib/externalDependencies/installDependencies.sh
@@ -1,5 +1,4 @@
 mvn install:install-file -Dfile=miniEugene.jar -DgroupId=org.cidarlab -DartifactId=miniEugene -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=iBioSim.jar -DgroupId=org -DartifactId=ibiosim -Dversion=2.9 -Dpackaging=jar
-mvn install:install-file -Dfile=JavaPlot.jar -DgroupId=org.java.plot -DartifactId=JavaPlot -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=STLSharp.jar -DgroupId=org.cidarlab.bioCPS -DartifactId=STLSharp -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=GridTLI.jar -DgroupId=org.cidarlab -DartifactId=GridTLI -Dversion=1.0 -Dpackaging=jar
