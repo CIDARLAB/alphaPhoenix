7f55f5a8df39136cd74f16e25827585646604ff0
PrashantVaidyanathan <prash@bu.edu>
2018-09-19 22:37:43 -0400

    scatter plot added

--- src/test/java/org/cidarlab/phoenix/utils/AnalyticsTest.java
+++ src/test/java/org/cidarlab/phoenix/utils/AnalyticsTest.java
@@ -21,10 +21,7 @@ public class AnalyticsTest {
     
     private final String basefp = Utilities.getLibFilepath() + "computationalTests" + Utilities.getSeparater();
     
-    
-    
-    
-    public void generateScript(String fp, String assignmentfp, String filename){
+    private void generateRobSMCScript(String fp, String assignmentfp, String filename){
         List<String[]> lines = Utilities.getCSVFileContentAsList(fp);
         List<String> script = new ArrayList<>();
         
@@ -53,6 +50,10 @@ public class AnalyticsTest {
  
         script.add("ln1 = ax1.plot(x,y1,'g-', label = 'SMC')");
         script.add("ln2 = ax2.plot(x,y2,'b-', label = 'Rob')");
+        
+        script.add("ax2.spines['bottom'].set_position('zero')");
+        script.add("ax2.spines['bottom'].set_linestyle(':')");
+        
         script.add("ax1.set_xlabel('Assignment Index')");
         script.add("ax1.set_ylabel('Satisfying Percentage')");
         script.add("ax2.set_ylabel('Robustness')");
@@ -62,6 +63,47 @@ public class AnalyticsTest {
         script.add("fig.savefig('" + assignmentfp + filename + ".png', dpi=300)");
         
         Utilities.writeToFile(assignmentfp + filename + ".py", script);
+        
+    }
+    
+    
+    private void generateScoreScatterPlot(List<String> fplist, String assignmentfp){
+        
+        List<String> script = new ArrayList<>();
+        
+        script.add("import matplotlib.pyplot as plt");
+        script.add("fig = plt.figure()");
+        List<String[]> lines = new ArrayList<>();
+        
+        for(String fp:fplist){
+             lines.addAll(Utilities.getCSVFileContentAsList(fp));
+        }
+        
+        String x = "";
+        String y = "";
+        
+        for(int i=0;i<lines.size()-1;i++){
+            x += lines.get(i)[3] + ",";
+            y += lines.get(i)[2] + ",";
+        }
+        x += lines.get(lines.size()-1)[3];
+        y += lines.get(lines.size()-1)[2];
+        
+        script.add("x = [" + x + "]");
+        script.add("y = ["+ y + "]");
+        script.add("ax = fig.add_subplot(111)");
+ 
+        script.add("ln = ax.plot(x,y,'b.')");
+        //script.add("ln = ax.scatter(x,y)");
+        
+        //script.add("ax.spines['bottom'].set_position('zero')");
+        //script.add("ax.spines['bottom'].set_linestyle('-')");
+        
+        script.add("ax.set_xlabel('Satisfying Percentage')");
+        script.add("ax.set_ylabel('Robustness')");
+        script.add("fig.savefig('" + assignmentfp + "scatter.png', dpi=300)");
+        
+        Utilities.writeToFile(assignmentfp + "scatter.py", script);
         
     }
     
@@ -88,9 +130,8 @@ public class AnalyticsTest {
         os.close();
     }
     
-    public void generateFiguresTest() throws IOException, InterruptedException{
-        double xthresh = 50;
-        double ythresh = 10000;
+    public void generateFiguresTest(double xthresh, double ythresh) throws IOException, InterruptedException{
+        
         
         String resultsfp = basefp + (xthresh + "_" + ythresh);
        
@@ -104,13 +145,19 @@ public class AnalyticsTest {
                 if(!assignmentfp.endsWith("" + Utilities.getSeparater())){
                     assignmentfp += Utilities.getSeparater();
                 }
-                generateScript(assignmentfp + "1tuScores.csv", assignmentfp, "oneTUscores");
-                generateScript(assignmentfp + "2tuScores.csv", assignmentfp, "twoTUscores");
+                generateRobSMCScript(assignmentfp + "1tuScores.csv", assignmentfp, "oneTUscores");
+                generateRobSMCScript(assignmentfp + "2tuScores.csv", assignmentfp, "twoTUscores");
+                
+                List<String> fplist = new ArrayList<>();
+                fplist.add(assignmentfp + "1tuScores.csv");
+                fplist.add(assignmentfp + "2tuScores.csv");
+                
+                generateScoreScatterPlot(fplist,assignmentfp);
                 
                 executeScript(assignmentfp + "oneTUscores.py");
                 executeScript(assignmentfp + "twoTUscores.py");
                 
-                
+                executeScript(assignmentfp + "scatter.py");
             }
             
         }
@@ -119,8 +166,15 @@ public class AnalyticsTest {
     
     
     public static void main(String[] args) throws IOException, InterruptedException{
+        
+        double xthresh = 50;
+        double ythresh = 1000;
+        
         AnalyticsTest analytics = new AnalyticsTest();
-        analytics.generateFiguresTest();
+        analytics.generateFiguresTest(xthresh,ythresh);
+        
+        
+        
     }
     
 }
